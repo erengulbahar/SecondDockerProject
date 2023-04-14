efd1e047a68e8a9fedf457c5876546b36c693159
Eren Gülbahar <erenngulbahar@gmail.com>
2023-04-14 18:32:00 +0300

    My Second Docker Project

---
+++ Homework2/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hello,\n\nThis is my second Docker file!\n\nIf you see this messages, this works successfully!\n\nI hope you will enjoy!!\n");	
+	
+	return 0;
+}
